0178f04dd26b8c64ec5f0f42d9de6a3151fbbd63
Matthew Zierl <matthewtzierl@gmail.com>
2024-05-12 14:00:28 +0900

    Initial Commit

---
+++ IWasHere/IWasHereApp.swift
@@ -0,0 +1,17 @@
+//
+//  IWasHereApp.swift
+//  IWasHere
+//
+//  Created by Matthew Zierl on 5/12/24.
+//
+
+import SwiftUI
+
+@main
+struct IWasHereApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
